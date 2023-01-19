4614636db6d845addc7c5f14699d01bfd00d06d0
USER <USER@DOMAIN.com>
2023-01-19 20:18:19 +0000

    .

---
+++ Git.bat
@@ -0,0 +1,21 @@
+@echo on
+set check_computername=%COMPUTERNAME%
+echo "Found computer %check_computername%"
+
+set PACKAGES="C:\Packages"
+set WPUBLIC="C:\wPublic"
+set SCRIPTS="C:\Scripts"
+set GIT="C:\Program Files\Git\git-bash.exe"
+
+mkdir %SCRIPTS%
+
+
+if exist %GIT% (
+   echo "Found git. Attempting pull"
+   cd %SCRIPTS%\wBase
+   %GIT% -c "git pull"
+
+   cd %SCRIPTS%\wLocal
+   %GIT% -c "git pull"
+
+)
