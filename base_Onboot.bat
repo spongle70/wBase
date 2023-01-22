df1ca116c68956c50af8e6082d384fc3d503d441
USER <USER@DOMAIN.com>
2023-01-22 18:04:22 +0000

    .

--- base_Onboot.bat
+++ base_Onboot.bat
@@ -6,9 +6,10 @@ echo "START: %~n0"
 echo "Fixing time...."
 call %BASE%\baseFixTime.bat
 
-call %TMESSAGE% '"...STARTED base %check_computername% "'
-
 del C:\Scripts\bSecrets\b_Syncthing.bat
 del C:\Scripts\lSecrets\l_Syncthing.bat
+
+call %TMESSAGE% '"...STARTED base %check_computername% "'
+
 
 echo "FINISHED: %~n0"
