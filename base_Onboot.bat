ab6dc35ff4da8a27d612752d6c35553b34cc7c0d
USER <USER@DOMAIN.com>
2023-01-28 12:56:20 +0000

    .

--- base_Onboot.bat
+++ base_Onboot.bat
@@ -9,6 +9,7 @@ call %BASE%\baseFixTime.bat
 del C:\Scripts\bSecrets\b_Syncthing.bat
 del C:\Scripts\lSecrets\l_Syncthing.bat
 
+ping -n 7 -i 7 192.168.1.1
 call %TMESSAGE% '"...STARTED base %check_computername% "'
 
 
