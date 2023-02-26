83964da1226f8bd7ab0fa75c68f63a4c55d4e3fa
USER <USER@DOMAIN.com>
2023-02-26 20:48:44 +0000

    .

--- base_Logon.bat
+++ base_Logon.bat
@@ -8,21 +8,37 @@ set LOCAL_LOGON=%LOCAL%\local_Logon.bat
 del C:\Scripts\bSecrets\b_Syncthing.bat
 del C:\Scripts\lSecrets\l_Syncthing.bat
 
-call C:\Scripts\wBase\BaseGit.bat
+set ZUPER=0
 
-FOR %%x IN (%BASE%\b_*.bat) DO (
-    echo "%%x"
-    call "%%x"
-)
+if "%USERNAME%" == "zuper" (
+    echo "Found user %USERNAME%"
+	echo "Not running Base scripts for %USERNAME%"
+    set ZUPER=1
+    )
 
-FOR %%x IN (C:\Scripts\bSecrets\b_*.bat) DO (
-    echo "%%x"
-    call "%%x"
-)
 
-if exist %LOCAL_LOGON% (
-    echo "Starting LOCALISED logon %LOCAL_LOGON% "
-    call %LOCAL_LOGON%
-)
+
+if [%ZUPER%] == [1] (
+	echo "Not running Base scripts for %USERNAME%"
+) else (
+        @REM if [%PRINTER%] == [1] (
+        @REM     echo "Installing Brother"
+        @REM     %EXE% -ExecutionPolicy Bypass -File %BROTHERPS%
+        @REM )
+
+        call C:\Scripts\wBase\BaseGit.bat
+        FOR %%x IN (%BASE%\b_*.bat) DO (
+            echo "%%x"
+            call "%%x"
+        )
+        FOR %%x IN (C:\Scripts\bSecrets\b_*.bat) DO (
+            echo "%%x"
+            call "%%x"
+        )
+        if exist %LOCAL_LOGON% (
+            echo "Starting LOCALISED logon %LOCAL_LOGON% "
+            call %LOCAL_LOGON%
+        )
+    )
 
 echo "FINISHED: %~n0"
